87031f30d4d40a98ef8b4eeaec780ef3c8962b6d
rosaliacondoricondo <rosaliacondoricondo@gmail.com>
2021-10-25 23:47:09 -0400

    practica6fin

---
+++ src/Movies.js
@@ -0,0 +1,14 @@
+import movies from "./movies.json";
+import axios from 'axios';
+console.log(movies);
+
+
+export function Movies() { 
+
+componentDidMount()
+axios.get()
+.then(result => {
+console.log(result)
+})
+.catch(console.log)
+};
